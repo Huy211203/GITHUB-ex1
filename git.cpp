788d2aa7a5b65e2f1c8ced45e116d6dbbb268c37
Huy211203 <101450582+Huy211203@users.noreply.github.com>
2022-03-12 15:50:14 +0700

    Add files via upload

---
+++ git.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+	cout << "Ho va ten : Le Nguyen Duc Huy" << endl;
+	cout << "MSSV : 106210126" << endl;
+	cout << "lop : 21DTCLC2";
+	return 0;
+}
